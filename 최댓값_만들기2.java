894918774d482ead6f55456cc0d50a39170248b2
李鍾完（カネヒロ） <52557269+LEEJ0NGWAN@users.noreply.github.com>
2023-02-26 23:36:08 +0900

    Create 최댓값_만들기2.java

---
+++ 최댓값_만들기2.java
@@ -0,0 +1,9 @@
+class Solution {
+
+    public int solution(int[] numbers) {
+
+        java.util.Arrays.sort(numbers);
+
+        return Math.max(numbers[0]*numbers[1],numbers[numbers.length-2]*numbers[numbers.length-1]);
+    }
+}
