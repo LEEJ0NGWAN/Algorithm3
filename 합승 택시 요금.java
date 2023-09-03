e118f7654b1922aadf3997515eb3a25d75d676ab
李鍾完（カネヒロ） <52557269+LEEJ0NGWAN@users.noreply.github.com>
2023-09-03 16:30:01 +0900

    Update 합승 택시 요금.java

--- 합승 택시 요금.java
+++ 합승 택시 요금.java
@@ -1,58 +1,28 @@
 class Solution {
 
+    private static final int INF = 2000000;
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
 
-        final int[][] edges = java.util.Arrays
+        final int[][] edges = new int[n+1][n+1];
+        for (int i=1; i<=n;
+             java.util.Arrays.fill(edges[i], INF), edges[i][i++] = 0);
+
+        java.util.Arrays
             .stream(fares)
-            .collect(
-                ()-> new int[n+1][n+1],
-                (arr, fare)->
-                    arr[fare[0]][fare[1]] =
-                    arr[fare[1]][fare[0]] = fare[2],
-                (arr1, arr2)-> {});
+            .forEach(fare->
+                    edges[fare[0]][fare[1]] =
+                    edges[fare[1]][fare[0]] = fare[2]);
 
-        final int[] commonDistance = dijkstra(n, s, edges);
-
-        int answer = Integer.MAX_VALUE;
-
+        for (int k=1; k<=n; k++)
         for (int i=1; i<=n; i++)
-        if (commonDistance[i]<Integer.MAX_VALUE) {
+        for (int j=1; j<=n; j++)
+        edges[i][j] = Math.min(edges[i][j], edges[i][k]+edges[k][j]);
 
-            final int[] eachDistance =
-                i==s? commonDistance: dijkstra(n, i, edges);
-
-            final int totalCost =
-                commonDistance[i]+eachDistance[a]+eachDistance[b];
-
-            answer = Math.min(answer, totalCost);
-        }
-
-        return answer;
-    }
-
-    public int[] dijkstra(int n, int start, int[][] edges) {
-
-        final int[] dist = new int[n+1];
-        for (int i=1; i<=n; dist[i++] = Integer.MAX_VALUE);
-
-        final java.util.PriorityQueue<Integer> pq =
-        new java.util.PriorityQueue<>((a,b)-> Integer.compare(dist[b], dist[a]));
-
-        pq.offer(start);
-        dist[start] = 0;
-
-        while (!pq.isEmpty()) {
-
-            final int now = pq.poll();
-            for (int i=1; i<=n; i++)
-            if (edges[now][i]!=0)
-            if (dist[i] > dist[now]+edges[now][i]) {
-
-                dist[i] = dist[now]+edges[now][i];
-                pq.offer(i);
-            }
-        }
-
-        return dist;
+        return java.util.stream.IntStream
+            .range(1,n+1)
+            .map(i -> edges[s][i]+edges[i][a]+edges[i][b])
+            .min()
+            .getAsInt();
     }
 }
